fedef9160f75fb231ea55e67bd50faad716cbce1
paulogiacomelli <paulogiacomelli@hotmail.com>
2022-03-24 12:56:44 -0600

    Requiring django on setup.py

--- setup.py
+++ setup.py
@@ -32,4 +32,7 @@ setup(name='django-activity-stream',
       extras_require={
           'jsonfield': ['django-jsonfield-backport>=1.0.2,<2.0'],
       },
+      install_requires={
+          'django': ['django>=2.2']
+      }
       )
